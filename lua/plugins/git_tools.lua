ad85027d2ec5a22ae125d105587dc044aff396ff
Dagl1 <jellebonthuis@hotmail.com>
2025-10-29 14:37:31 +0100

    added VGit plugin and dependencies, and added vim-fugitive for additional git functionality

---
+++ lua/plugins/git_tools.lua
@@ -0,0 +1,11 @@
+return {
+    {"tpope/vim-fugitive", },
+    {
+      'tanvirtin/vgit.nvim',
+      dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+      -- Lazy loading on 'VimEnter' event is necessary.
+      event = 'VimEnter',
+      config = function() require("vgit").setup() end,
+    }
+    
+}
