980f96ccbc0b68d774b55b8cebce00368ad471f8
Dagl1 <jellebonthuis@hotmail.com>
2025-11-05 15:08:15 +0100

    style(git_tools.lua): added live-gutter option default to negative

--- lua/plugins/git_tools.lua
+++ lua/plugins/git_tools.lua
@@ -6,6 +6,10 @@ return {
       -- Lazy loading on 'VimEnter' event is necessary.
       event = 'VimEnter',
       config = function() require("vgit").setup() end,
+      settings = {
+        live_gutter = {
+          toggle_live_gutter = false,  -- disable live gutter on startup
+        },
     }
     
 }
