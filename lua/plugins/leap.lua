ef9133ecb85826efe0f1b468d44aa255f474e29c
dagl1 <jellebonthuis@hotmail.com>
2025-12-04 22:24:13 +0100

    build(plugins): now includes leap.lua

---
+++ lua/plugins/leap.lua
@@ -0,0 +1,13 @@
+return {
+   "ggandor/leap.nvim",
+  config = function()
+    local leap = require("leap")
+    leap.add_default_mappings()
+
+    -- ✅ Force label mode instead of instant jump
+    leap.opts.safe_labels = {}
+    leap.opts.labels = "asdfghjklqwertyuiopzxcvbnm"
+    vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+    vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-from-window)")
+  end,
+}
